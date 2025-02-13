79c4f19899b1b0be8dbe8fda65dce25b7f69faea
Huy Hoang Hoang <u7671528@anu.edu.au>
2025-02-13 23:23:27 +0700

    Fix misc errors

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -2,6 +2,7 @@
 # import requests
 import os
 from dotenv import load_dotenv
+import requests
 
 load_dotenv()
 
@@ -18,7 +19,7 @@ def get_request(endpoint, **kwargs):
             params=params + key + "=" + value + "&"
     request_url = backend_url+endpoint + "?" + params
 
-    print("GET from {} ".format(request_url))4
+    print("GET from {} ".format(request_url))
 
     try:
         # Call get method of requests library with URL and parameters
